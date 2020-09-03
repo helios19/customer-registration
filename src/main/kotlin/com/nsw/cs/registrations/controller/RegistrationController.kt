189744a85cc3af60901d2a11f78c2ea35b66dec8
helios <prenou19@gmail.com>
2020-09-04 06:28:54 +1000

    Add more comments and clean up source code

--- src/main/kotlin/com/nsw/cs/registrations/controller/RegistrationController.kt
+++ src/main/kotlin/com/nsw/cs/registrations/controller/RegistrationController.kt
@@ -33,57 +33,7 @@ class RegistrationController {
 
         return ResponseEntity
                 .ok(RegistrationList(registrations))
-
-
-//        Resources<Resource<Order>> all() {
-//
-//            List<Resource<Order>> orders = orderRepository.findAll().stream()
-//                    .map(assembler::toResource)
-//                    .collect(Collectors.toList());
-//
-//            return new Resources<>(orders,
-//            linkTo(methodOn(OrderController.class).all()).withSelfRel());
-
-
-//        return ResponseEntity
-//                .ok()
-//                .body<List<Registration>>(registrations)
-
     }
-
-//    @GetMapping("/{slug}")
-//    fun findOne(@PathVariable slug: String) =
-//            repository.findBySlug(slug) ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
-
 }
-
-
-//import org.springframework.http.HttpStatus.*
-//import org.springframework.web.bind.annotation.*
-//import org.springframework.web.server.ResponseStatusException
-//
-//@RestController
-//@RequestMapping("/api/article")
-//class ArticleController(private val repository: ArticleRepository) {
-//
-//    @GetMapping("/")
-//    fun findAll() = repository.findAllByOrderByAddedAtDesc()
-//
-//    @GetMapping("/{slug}")
-//    fun findOne(@PathVariable slug: String) =
-//            repository.findBySluepr   g(slug) ?: throw ResponseStatusException(NOT_FOUND, "This article does not exist")
-//
-//}
-//
-//@RestController
-//@RequestMapping("/api/user")
-//class UserController(private val repository: UserRepository) {
-//
-//    @GetMapping("/")
-//    fun findAll() = repository.findAll()
-//
-//    @GetMapping("/{login}")
-//    fun findOne(@PathVariable login: String) = repository.findByLogin(login) ?: throw ResponseStatusException(NOT_FOUND, "This user does not exist")
-//}
 
 
